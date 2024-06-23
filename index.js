8d8875e374efcf3be7dd767fec34f312508f4c5b
HIBA KHAN <161249784+hibakashifkhan@users.noreply.github.com>
2024-06-23 13:34:04 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,44 @@
+#! usr/bin/env node
+import inquirer from "inquirer";
+let BankBalance = 50000;
+let myPinCode = 3214;
+console.log("Welcome to Hiba's ATM Machine");
+let PinAnswer = await inquirer.prompt([
+    {
+        name: "pin",
+        type: "number",
+        message: "Please Enter Your PinCode :"
+    }
+]);
+if (PinAnswer.pin === myPinCode) {
+    console.log("Access Granted!");
+    console.log(`Current Bank Balance = ${BankBalance}`);
+    let operationAns = await inquirer.prompt([
+        {
+            name: "operation",
+            type: "list",
+            message: "Please select an operator",
+            choices: ["Withdraw Amount", "Check Bank Balance"]
+        }
+    ]);
+    if (operationAns.operation === "Withdraw Amount") {
+        let amountAns = await inquirer.prompt([
+            {
+                name: "amount",
+                type: "number",
+                message: "Please enter the withdrawal amount"
+            }
+        ]);
+        if (amountAns.amount > BankBalance) {
+            console.log("Insufficient Balance");
+        }
+        else {
+            BankBalance -= amountAns.amount;
+            console.log(`${amountAns.amount} Withdraw Successfull`);
+            console.log(`Remaining Balance = ${BankBalance}`);
+        }
+    }
+    else if (operationAns.operation === "Check Bank Balance") {
+        console.log(`Your account balance is ${BankBalance}`);
+    }
+}
